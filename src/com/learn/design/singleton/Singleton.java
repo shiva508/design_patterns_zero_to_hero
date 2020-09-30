716dbded28b5ac3eebbd9d6a60cef1702591472a
shiva508 <dasarishiva1@gmail.com>
2020-09-30 14:13:53 +0530

    Singleton design pattern

---
+++ src/com/learn/design/singleton/Singleton.java
@@ -0,0 +1,5 @@
+package com.learn.design.singleton;
+
+public class Singleton {
+
+}
---
+++ src/com/learn/design/singleton/SingletonWithFinal.java
@@ -0,0 +1,14 @@
+package com.learn.design.singleton;
+
+public class SingletonWithFinal {
+	private static final SingletonWithFinal SINGLETON_WITH_FINAL = new SingletonWithFinal();
+
+	private SingletonWithFinal() {
+
+	}
+
+	public static SingletonWithFinal getSingletonWithFinal() {
+		return SINGLETON_WITH_FINAL;
+	}
+
+}
