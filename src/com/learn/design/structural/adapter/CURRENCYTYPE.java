b3150e70cecdad6258c5ef0b5113e49cbca8c786
shiva508 <dasarishiva1@gmail.com>
2021-07-10 00:20:45 +0530

    Structural:Adapter

---
+++ src/com/learn/design/structural/adapter/CURRENCYTYPE.java
@@ -0,0 +1,5 @@
+package com.learn.design.structural.adapter;
+
+public enum CURRENCYTYPE {
+	INDIA, RUSSIA, SPAIN
+}
---
+++ src/com/learn/design/structural/adapter/StandardCurrencyRate.java
@@ -0,0 +1,7 @@
+package com.learn.design.structural.adapter;
+
+public class StandardCurrencyRate {
+	public Currency getStandardCurrencyRate() {
+		return new Currency(100.00);
+	}
+}
